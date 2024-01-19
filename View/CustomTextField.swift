4ea8eae8d9ebb32ebdd86335341cd07ac04c6027
Bryan Battu <bryan.battu@gmail.com>
2024-01-19 22:25:26 +0100

    init

---
+++ View/CustomTextField.swift
@@ -0,0 +1,66 @@
+//
+//  CustomTextField.swift
+//  MuscleUp
+//
+//  Created by Bryan Battu on 14/01/2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    var sfIcon: String
+    var iconTint: Color = .gray
+    var hint: String
+    /// Hides TextField
+    var isPassword: Bool = false
+    @Binding var value: String
+    
+    /// View properties
+    @State private var showPassword: Bool = false
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 8, content: {
+            Image(systemName: sfIcon)
+                .foregroundStyle(iconTint)
+                .frame(width: 30)
+                .offset(y: 2)
+            
+            VStack(alignment: .leading, spacing: 8, content: {
+                // Si c'est un mot de passe alors on affiche un textField secure
+                // Sinon on affiche un textField classique
+                if isPassword {
+                    Group {
+                        /// Revealing password when users wants to show password
+                        if showPassword {
+                            TextField(hint, text: $value)
+                        } else {
+                            SecureField(hint, text: $value)
+                        }
+                    }
+                } else {
+                    TextField(hint, text: $value)
+                }
+                
+                Divider()
+            })
+            
+            .overlay(alignment: .trailing){
+                /// Password reveal button
+                if isPassword {
+                    Button(action: {
+                        withAnimation {
+                            showPassword.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: showPassword ? "eye.slash" : "eye")
+                            .foregroundStyle(.gray)
+                            .padding(10)
+                            .contentShape(.rect)
+                    })
+                }
+            }
+        })
+    }
+}
+
